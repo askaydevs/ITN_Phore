bedd3327068056084fad9bafe89ed2f3014beba5
Akashsuryak <48252954+Akashsuryak@users.noreply.github.com>
2021-06-09 15:35:00 +0530

    Update Data_Set_Generator.py

--- asr_ask/scripts/Data_Set_Generator.py
+++ asr_ask/scripts/Data_Set_Generator.py
@@ -1,26 +1,32 @@
 #importing packages
 import codecs
 import soundfile as sf
+from torch._C import import_ir_module
 from nemo.collections.tts.models.base import SpectrogramGenerator, Vocoder
 import os
-import json
+from glob import glob
 import librosa
+import json
+import nemo.collections.asr as nemo_asr
+
 
 class datagenerator():
     
     #initialization of file locations and pre-trained model for audio generation
-    def __init__(self, root, train, target, audio, manifest):
-        self.dataset = root
-        self.train = train
-        self.target = target
-        self.audio = audio
-        self.manifest_path = manifest
+    def __init__(self, root, train, target, audio, transcribe):
+        self.dataset = root #lang-8 en file location
+        self.train = train #path to grammtically incorrect sentences
+        self.target = target #path to grammatically correct sentences
+        self.audio = audio #path to audio files storage location
+        self.transcribe = transcribe #path to store transcibed sentences from audio files
+        self.audio_files = []
         self.count = 1
 
         #text-to-speech can be done by converting text to spectrogram and then to audio.
         #tacoton-2 model is used to generate spectrogram and hifigan is used for audio file generation from spectrogram
-        self.spectro = SpectrogramGenerator.from_pretrained('tts_en_tacotron2')
-        self.vocoder = Vocoder.from_pretrained('tts_hifigan').eval().cuda()
+        self.spectro = SpectrogramGenerator.from_pretrained('tts_en_tacotron2') #for spectrogram generation
+        self.vocoder = Vocoder.from_pretrained('tts_hifigan').eval().cuda() #for audio generation from spectrogram
+        self.citrinet = nemo_asr.models.EncDecCTCModelBPE.from_pretrained(model_name="stt_en_citrinet_1024") #asr pre-trained model
 
     #function obtains grammatically incorrect and correct sentences
     def text_process(self):
@@ -35,12 +41,12 @@ class datagenerator():
                     corr = cols[5]
 
                 #orig is the original grammatically incorrect sentence and corr is the grammtically correct sentence
-                ftt.write(orig + "\n")
-                ft.write(corr + "\n")
+                ftt.write(orig+ "\n")
+                ft.write(corr+ "\n")
     
     #function generates audio files from grammatically incorrect sentences
     def audio_process(self):
-        with codecs.open(self.train, 'r', encoding='utf8') as fin, codecs.open(self.manifest_path, 'w') as fout:
+        with codecs.open(self.train, 'r', encoding='utf8') as fin:
             for line in fin:
 
                 parsed = self.spectro.parse(line)
@@ -48,21 +54,26 @@ class datagenerator():
                 audio = self.vocoder.convert_spectrogram_to_audio(spec=spectrogram)[0]
                 
                 audio = audio.to('cpu').detach().numpy()
-                audio_path = os.path.join(self.audio, str("audio_spec2wav_sample_"+ str("{:04d}".format(self.count))+ ".wav"))
+                audio_path = os.path.join(self.audio, str("audio_spec2wav_sample_"+ str("{:07d}".format(self.count))+ ".wav"))
 
                 sf.write(audio_path, audio, 22050)
 
-                duration = librosa.core.get_duration(filename=audio_path)
-
-                metadata = {
-                    "audio_filepath": audio_path,
-                    "duration": duration,
-                    "text": line
-                }
-                json.dump(metadata, fout)
-                fout.write('\n')
-
                 self.count+= 1
+        
+    #function used for automatic speech recognition
+    def speech_recognition(self):
+        #obtain all audio files
+        self.audio_files = sorted(glob(os.path.join(self.audio, '*.wav')), key= lambda x: x[-11:-4])
+        batch_size = 10
+
+        with codecs.open(self.transcribe, 'w', encoding='utf8') as f:
+            for i in range(len(self.audio_files)//batch_size):
+                batch = self.audio_files[i*batch_size: (i+1)*batch_size]
+
+                output = self.citrinet.transcribe(batch)
+
+                for a in output:
+                    f.write(a + '\n')
 
 
 if __name__ == '__main__':
@@ -73,11 +84,13 @@ if __name__ == '__main__':
     train_train = '/path/train/transcript/train.train'
     target_train = 'path/train/transcript/target.train'
     audio_train = 'path/train/audio'
-    manifest = '/path/train/transcript/train_dataset.json'
+    transcribe = 'path/Transcribed/train/transcript.train'
 
     #declaring object of class datagenerator
-    obj = datagenerator(dataset_root, train_train, target_train, audio_train, manifest)
+    obj = datagenerator(dataset_root, train_train, target_train, audio_train, transcribe)
     #function used to obtain grammatically incorrect and correct sentences from raw data
     obj.text_process()
     #function used to convert incorrect sentences to audio format for training ASR model
     obj.audio_process()
+    #function used to transcribe audio files into text
+    obj.speech_recognition()
