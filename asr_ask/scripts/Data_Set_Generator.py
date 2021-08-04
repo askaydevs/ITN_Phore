4cbad0af6c0f5b7637ffbd56a09cab880d2c31ea
Akashsuryak <48252954+Akashsuryak@users.noreply.github.com>
2021-08-04 11:34:14 +0530

    Update Data_Set_Generator.py

--- asr_ask/scripts/Data_Set_Generator.py
+++ asr_ask/scripts/Data_Set_Generator.py
@@ -21,8 +21,8 @@ class datagenerator():
 
         #text-to-speech can be done by converting text to spectrogram and then to audio.
         #tacoton-2 model is used to generate spectrogram and hifigan is used for audio file generation from spectrogram
-        self.spectro = SpectrogramGenerator.from_pretrained('tts_en_tacotron2').eval().cuda() #for spectrogram generation
-        self.vocoder = Vocoder.from_pretrained('tts_hifigan').eval().cuda() #for audio generation from spectrogram
+        #self.spectro = SpectrogramGenerator.from_pretrained('tts_en_tacotron2').eval().cuda() #for spectrogram generation
+        #self.vocoder = Vocoder.from_pretrained('tts_hifigan').eval().cuda() #for audio generation from spectrogram
         self.citrinet = nemo_asr.models.EncDecCTCModelBPE.from_pretrained(model_name="stt_en_citrinet_1024").eval().cuda() #asr pre-trained model
 
     #function obtains grammatically incorrect and correct sentences
@@ -61,7 +61,7 @@ class datagenerator():
     def speech_recognition(self):
         #obtain all audio files
         self.audio_files = sorted(glob(os.path.join(self.audio, '*.wav')), key= lambda x: x[-11:-4])
-        batch_size = 10
+        batch_size = 1000
 
         with codecs.open(self.transcribe, 'w', encoding='utf8') as f:
             for i in range(len(self.audio_files)//batch_size):
@@ -80,18 +80,18 @@ if __name__ == '__main__':
     for i in files:
 
         #path to lang-8 dataset, grammatically incorrect sentences, correct sentences and audio files respectively
-        dataset_root = 'path'+ i+ '/transcript/sample_entries'
-        train = 'path'+ i+ '/transcript/train'
-        target = 'path'+ i+ '/transcript/target'
-        audio = 'path'+ i+ '/audio'
-        transcribe = 'path'+ i+  '/transcribed/transcribed'
+        dataset_root = path+ i+ '/transcript/sample_entries'
+        train = path+ i+ '/transcript/train'
+        target = path+ i+ '/transcript/target'
+        audio = path+ i+ '/audio'
+        transcribe = path+ i+  '/transcribed/transcribed'
 
 
         #declaring object of class datagenerator
         obj = datagenerator(dataset_root, train, target, audio, transcribe)
         #function used to obtain grammatically incorrect and correct sentences from raw data
-        obj.text_process()
+        #obj.text_process()
         #function used to convert incorrect sentences to audio format for training ASR model
-        obj.audio_process()
+        #obj.audio_process()
         #function used to transcribe audio files into text
         obj.speech_recognition()
